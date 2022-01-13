6435c5ff12aebeafc7ba18f4552722a272b529ff
Anatr1 <fedemustik@gmail.com>
2022-01-13 19:43:09 +0100

    First commit

---
+++ 2D Multiplayer Space Shooter/Assets/Scripts/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants {
+
+    public static readonly string GAMEOVER_SCENE = "03_Scores";
+    public static readonly int LAYER_OFFSET = 8;
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+# multiplayer_spaceshooter
+
+A simple multiplayer space-shooter videogame developed on Unity that uses smartphones as controllers, so you can play with your friends in LAN!
